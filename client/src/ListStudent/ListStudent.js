91e36360b76c0aa2568050a7bffeb147f5004713
Bùi Minh Hiếu <62604308+minhhieu3001@users.noreply.github.com>
2022-07-31 18:59:29 +0700

    eee

--- client/src/ListStudent/ListStudent.js
+++ client/src/ListStudent/ListStudent.js
@@ -38,6 +38,10 @@ class ListStudent extends Component {
       } else {
         this.setState({
           students: [],
+
+
+
+          
         });
       }
       console.log(this.state.students);
